7d4f6fc00f8f5316e441ccfcd591bbf95fd8dba9
Roy Chanunkha <roychanunkha@gmail.com>
2022-10-03 12:24:40 +0200

    payload updated

--- monitor.sh
+++ monitor.sh
@@ -28,7 +28,7 @@ function send_data_to_api {
   echo "Sending data to api"
   echo "-------------------"
   echo "Data to send: $1"
-  response=$(curl --write-out '%{http_code}' --output /dev/null -s -X POST -H "Content-Type: application/json" -d '{"data": "'$1'"}}' $api)
+  response=$(curl --write-out '%{http_code}' --output /dev/null -s -X POST -H "Content-Type: application/json" -d "'$1'" $api)
   if [ $response -eq 200 ]; then
     echo "Data sent successfully"
   else
@@ -38,17 +38,17 @@ function send_data_to_api {
 }
 
 function update_failed_records_in_database {
-  sqlite3 ./log/transaction.db "$1"
+  sqlite3 /opt/egpaf/monitor/log/transaction.db "$1"
 }
 
 function delete_synced_records_in_database {
-  sqlite3 ./log/transaction.db "DELETE FROM transactions WHERE sync_status = 1;"
+  sqlite3 /opt/egpaf/monitor/log/transaction.db "DELETE FROM transactions WHERE sync_status = 1;"
 }
 
 # process all records not synced
 function process_records {
   # get all records not synced
-  records=$(sqlite3 -json ./log/transaction.db "SELECT * FROM transactions where sync_status = 0 LIMIT 30;")
+  records=$(sqlite3 -json /opt/egpaf/monitor/log/transaction.db "SELECT * FROM transactions where sync_status = 0 LIMIT 30;")
   statements=""
   # loop through each record in the json array
 
@@ -93,30 +93,31 @@ function process_records {
 function failed_connection {
   # insert into sqlite database
   uuid=$(cat /proc/sys/kernel/random/uuid)
-  enddate=$(date)
-  sqlite3 ./log/transaction.db "INSERT INTO transactions (id, start_time, end_time, online, molecular_address, port, scan_status, sync_status) VALUES ('$uuid','$1','$enddate',0, '$checkml', '$checkport', $2, 0);"
+  enddate=$(date +"%Y-%m-%d %H:%M:%S")
+  sqlite3 /opt/egpaf/monitor/log/transaction.db "INSERT INTO transactions (id, start_time, end_time, online, molecular_address, port, scan_status, sync_status) VALUES ('$uuid','$1','$enddate',0, '$checkml', '$checkport', $2, 0);"
   echo "Failed connection"
 }
 
 function bandwidth {
-  startdate=$(date +%a,\ %d\ %b\ %Y\ %T)
-  iperf3 -c $MLABIP -bidir -J | tee ./log/test.json
+  # date should be in mysql standard format
+  startdate=$(date +"%Y-%m-%d %H:%M:%S")
+  iperf3 -c $MLABIP -bidir -J | tee /opt/egpaf/monitor/log/test.json
   scan=$(portscan)
-  startime=$(jq '.start.timestamp.time' ./log/test.json)
+  startime=$(jq '.start.timestamp.time' /opt/egpaf/monitor/log/test.json)
   # check if startime is null or empty
   if [ -z "$startime" ]; then
     failed_connection "$startdate" $scan
   else
     # create an end date in 24hr format
-    endtime=$(date +%a,\ %d\ %b\ %Y\ %T)
-    senderbits=$(jq '.end.sum_sent.bits_per_second' ./log/test.json)
-    receiverbits=$(jq '.end.sum_received.bits_per_second' ./log/test.json)
+    endtime=$(date +"%Y-%m-%d %H:%M:%S")
+    senderbits=$(jq '.end.sum_sent.bits_per_second' /opt/egpaf/monitor/log/test.json)
+    receiverbits=$(jq '.end.sum_received.bits_per_second' /opt/egpaf/monitor/log/test.json)
     # convert bits to megabits
     senderbits=$(convert_bit_to_megabit $senderbits)
     receiverbits=$(convert_bit_to_megabit $receiverbits)
     # insert into sqlite database
     uuid=$(cat /proc/sys/kernel/random/uuid)
-    sqlite3 ./log/transaction.db "INSERT INTO transactions (id, start_time, end_time, sender_bits, receiver_bits, online, molecular_address, port, scan_status, sync_status) VALUES ('$uuid','$startdate', '$endtime', $senderbits, $receiverbits, 1, '$checkml', '$checkport', '$scan', 0);"
+    sqlite3 /opt/egpaf/monitor/log/transaction.db "INSERT INTO transactions (id, start_time, end_time, sender_bits, receiver_bits, online, molecular_address, port, scan_status, sync_status) VALUES ('$uuid','$startdate', '$endtime', $senderbits, $receiverbits, 1, '$checkml', '$checkport', '$scan', 0);"
     # send data to api
     data="{\"site_id\":\"$site\",\"test_time\":\"$startdate\",\"uplink\":\"$senderbits\",\"downlink\":\"$receiverbits\",\"online\":1,\"scan_status\":$scan,\"ip_address\":\"$checkml\",\"port\":\"$checkport\"}"
     response= send_data_to_api "$data"
@@ -139,7 +140,7 @@ function portscan {
 
 while true; do
   # check if there are records not synced
-  records=$(sqlite3 -json ./log/transaction.db "SELECT * FROM transactions where sync_status = 0 LIMIT 1;")
+  records=$(sqlite3 -json /opt/egpaf/monitor/log/transaction.db "SELECT * FROM transactions where sync_status = 0 LIMIT 1;")
   if [[ $records != "[]" ]]; then
     # process the records
     process_records &
@@ -149,5 +150,6 @@ while true; do
   # run the bandwidth test
   bandwidth &
   # sleep for 5 minutes
+  echo "Sleeping for $interval seconds"
   sleep $interval
 done
