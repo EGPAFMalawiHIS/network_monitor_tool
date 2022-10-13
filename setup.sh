e498bd073f1840f3bb0ab1504c442ba8a2b1d83f
Roy Chanunkha <roychanunkha@gmail.com>
2022-10-13 12:01:02 +0200

    updated the logic to have common function to avoid unnecessary installations

--- setup.sh
+++ setup.sh
@@ -3,90 +3,142 @@
 # change mode uninstall.sh to executable
 sudo chmod +x uninstall.sh
 
-# check if egpaf monitor folder exists
-if [ ! -d /opt/egpaf/monitor ]; then
-    sudo mkdir -p /opt/egpaf/monitor
-    sudo chmod 777 /opt/egpaf
-    sudo chmod 777 /opt/egpaf/monitor
-fi
+function hubEnviromentSetup {
+    # check if egpaf monitor folder exists
+    if [ ! -d /opt/egpaf/monitor ]; then
+        sudo mkdir -p /opt/egpaf/monitor
+        sudo chmod 777 /opt/egpaf
+        sudo chmod 777 /opt/egpaf/monitor
+    fi
 
-# create log folder
-if [ ! -d /opt/egpaf/monitor/log ]; then
-    sudo mkdir -p /opt/egpaf/monitor/log
-    sudo chmod 777 /opt/egpaf/monitor/log
-fi
+    # create log folder
+    if [ ! -d /opt/egpaf/monitor/log ]; then
+        sudo mkdir -p /opt/egpaf/monitor/log
+        sudo chmod 777 /opt/egpaf/monitor/log
+    fi
 
-# remove .env file if it exists
-if [ -f ./.env ]; then
-    sudo rm ./.env
-fi
+    # remove .env file if it exists
+    if [ -f ./.env ]; then
+        sudo rm ./.env
+    fi
 
-# check if jq is installed
-if ! [ -x "$(command -v jq)" ]; then
-    echo 'jq is not installed.' >&2
-    echo 'Installing jq...'
-    sudo apt-get install jq
-    # exit if jq is not installed
-    if ! [ -x "$(command -v jq)" ]; then
-     # install jq from packages folder
-        sudo dpkg -i packages/libc6_2.27-3ubuntu1_amd64.deb
-        sudo dpkg -i packages/libonig4_6.7.0-1_amd64.deb
-        sudo dpkg -i packages/libjq1_1.5+dfsg-2_amd64.deb
-        sudo dpkg -i packages/jq_1.5+dfsg-2_amd64.deb
-        if ! [ -x "$(command -v jq)" ]; then
-            echo 'jq installation failed.' >&2
-            echo 'Please install jq manually and try again.' >&2
-            exit 1
+    # create monitor sqlite database file does not exists
+    if [ ! -f /opt/egpaf/monitor/log/transaction.db ]; then
+        sudo touch /opt/egpaf/monitor/log/transaction.db
+        sudo chmod 777 /opt/egpaf/monitor/log/transaction.db
+        # create table
+        sqlite3 /opt/egpaf/monitor/log/transaction.db "CREATE TABLE transactions (id TEXT PRIMARY KEY NOT NULL, start_time TEXT, end_time TEXT, sender_bits TEXT, receiver_bits TEXT, online INTEGER NOT NULL, molecular_address TEXT NOT NULL, port TEXT NOT NULL, scan_status INTEGER, sync_status INTEGER NOT NULL); CREATE INDEX idx_transactions_sync_status ON transactions (sync_status);"
+    fi
+
+    # remove monitor.service file if it exists
+    if [ -f ./monitor.service ]; then
+        sudo rm ./monitor.service
+    fi
+}
+
+function molecularEnvironmentSetup {
+    # remove server.service file if it exists
+    if [ -f ./server.service ]; then
+        sudo rm ./server.service
+    fi
+}
+
+function gitInstallation {
+    # check if git is installed
+    if ! [ -x "$(command -v git)" ]; then
+        echo 'git is not installed.' >&2
+        echo 'Installing git...'
+        sudo apt-get install git
+        if ! [ -x "$(command -v git)" ]; then
+            # install git from packages folder
+            sudo dpkg -i ./packages/git-all_2.24.0-1~nd18.04+1_all.deb
+            if ! [ -x "$(command -v git)" ]; then
+                echo 'git installation failed.' >&2
+                echo 'Please install git manually and try again.' >&2
+                exit 1
+            fi
         fi
     fi
-fi
+}
 
-# check if sqlite3 is installed
-if ! [ -x "$(command -v sqlite3)" ]; then
-    echo 'sqlite3 is not installed.' >&2
-    echo 'Installing sqlite3...'
-    sudo apt-get install sqlite3
-    # exit if sqlite3 is not installed
+function jqInstallation {
+    # check if jq is installed
+    if ! [ -x "$(command -v jq)" ]; then
+        echo 'jq is not installed.' >&2
+        echo 'Installing jq...'
+        sudo apt-get install jq
+        # exit if jq is not installed
+        if ! [ -x "$(command -v jq)" ]; then
+            # install jq from packages folder
+            sudo dpkg -i packages/libc6_2.27-3ubuntu1_amd64.deb
+            sudo dpkg -i packages/libonig4_6.7.0-1_amd64.deb
+            sudo dpkg -i packages/libjq1_1.5+dfsg-2_amd64.deb
+            sudo dpkg -i packages/jq_1.5+dfsg-2_amd64.deb
+            if ! [ -x "$(command -v jq)" ]; then
+                echo 'jq installation failed.' >&2
+                echo 'Please install jq manually and try again.' >&2
+                exit 1
+            fi
+        fi
+    fi
+}
+
+function sqliteInstallation {
+    # check if sqlite3 is installed
     if ! [ -x "$(command -v sqlite3)" ]; then
-        echo 'sqlite3 installation failed.' >&2
-        echo 'Please install sqlite3 manually and try again.' >&2
-        exit 1
+        echo 'sqlite3 is not installed.' >&2
+        echo 'Installing sqlite3...'
+        sudo apt-get install sqlite3
+        # exit if sqlite3 is not installed
+        if ! [ -x "$(command -v sqlite3)" ]; then
+            # install sqlite3 from packages folder
+            sudo dpkg -i packages/sqlite3_3.22.0-1_amd64.deb
+            if ! [ -x "$(command -v sqlite3)" ]; then
+                echo 'sqlite3 installation failed.' >&2
+                echo 'Please install sqlite3 manually and try again.' >&2
+                exit 1
+            fi
+        fi
     fi
-fi
+}
 
-# check if curl is installed
-if ! [ -x "$(command -v curl)" ]; then
-    echo 'curl is not installed.' >&2
-    echo 'Installing curl...'
-    sudo apt-get install curl
-    # exit if curl is not installed
+function curlInstallation {
+    # check if curl is installed
     if ! [ -x "$(command -v curl)" ]; then
-        echo 'curl installation failed.' >&2
-        echo 'Please install curl manually and try again.' >&2
-        exit 1
+        echo 'curl is not installed.' >&2
+        echo 'Installing curl...'
+        sudo apt-get install curl
+        # exit if curl is not installed
+        if ! [ -x "$(command -v curl)" ]; then
+            # install curl from packages folder
+            sudo dpkg -i packages/curl_7.58.0-2ubuntu3_amd64.deb
+            if ! [ -x "$(command -v curl)" ]; then
+                echo 'curl installation failed.' >&2
+                echo 'Please install curl manually and try again.' >&2
+                exit 1
+            fi
+        fi
     fi
-fi
+}
 
-# check if iperf3 is installed
-if ! [ -x "$(command -v iperf3)" ]; then
-    echo 'iperf3 is not installed.' >&2
-    echo 'Installing iperf3...'
-    # install local debian file
-    sudo dpkg -i ./iperf3_3.9-1_amd64.deb
-fi
-
-# create monitor sqlite database file does not exists
-if [ ! -f /opt/egpaf/monitor/log/transaction.db ]; then
-    sudo touch /opt/egpaf/monitor/log/transaction.db
-    sudo chmod 777 /opt/egpaf/monitor/log/transaction.db
-    # create table
-    sqlite3 /opt/egpaf/monitor/log/transaction.db "CREATE TABLE transactions (id TEXT PRIMARY KEY NOT NULL, start_time TEXT, end_time TEXT, sender_bits TEXT, receiver_bits TEXT, online INTEGER NOT NULL, molecular_address TEXT NOT NULL, port TEXT NOT NULL, scan_status INTEGER, sync_status INTEGER NOT NULL); CREATE INDEX idx_transactions_sync_status ON transactions (sync_status);"
-fi
-
-# remove monitor.service file if it exists
-if [ -f ./monitor.service ]; then
-    sudo rm ./monitor.service
-fi
+function iperfInstallation {
+    # check if iperf3 is installed
+    if ! [ -x "$(command -v iperf3)" ]; then
+        echo 'iperf3 is not installed.' >&2
+        echo 'Installing iperf3...'
+        sudo apt-get install iperf3
+        # exit if iperf3 is not installed
+        if ! [ -x "$(command -v iperf3)" ]; then
+            # install iperf3 from packages folder
+            sudo dpkg -i packages/iperf3_3.1.3-1_amd64.deb
+            if ! [ -x "$(command -v iperf3)" ]; then
+                echo 'iperf3 installation failed.' >&2
+                echo 'Please install iperf3 manually and try again.' >&2
+                exit 1
+            fi
+        fi
+    fi
+}
 
 # create an function that captures environment variables
 function capturEnv {
@@ -233,8 +285,17 @@ function createService {
 # Prompt user to select if they want to setup the hub or the molecular lab
 read -p "Do you want to setup the hub or the molecular lab? (h/m) " setup
 if [ "$setup" == "h" ]; then
+    gitInstallation
+    curlInstallation
+    iperfInstallation
+    jqInstallation
+    sqliteInstallation
+    hubEnviromentSetup
     createService
 elif [ "$setup" == "m" ]; then
+    gitInstallation
+    iperfInstallation
+    molecularEnvironmentSetup
     setupMolecularLab
 else
     echo "Invalid selection. Exiting setup"
