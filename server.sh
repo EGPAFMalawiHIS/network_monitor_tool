38ec9899ca5d5cb94f16c077fc062147a3258d67
Roy Chanunkha <roychanunkha@gmail.com>
2022-10-26 10:41:20 +0200

    will keep the service running

--- server.sh
+++ server.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+echo 'Starting iperf3 server...'
 iperf3 -s -D
+
+# loop every minute
+while true; do
+    # check if the iperf3 server is running
+    if ! pgrep -x "iperf3" > /dev/null
+    then
+        echo 'iperf3 server is not running.' >&2
+        echo 'Starting iperf3 server...'
+        iperf3 -s -D
+    fi
+    sleep 60
+done
